c6ec964e203859778d836dfb37912741e2ac8ec6
Alvaro Acevedo <alvaro.acevedo.ing@gmail.com>
2024-02-12 12:46:16 -0300

    se agregan display condicional a la barra de iconos, validadores del formulario de contacto y alertas

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,18 +1,56 @@
-/**
- * Listen to scroll to change header opacity class
- */
-function checkScroll(){
-  var startY = $('.navbar').height() * 2; //The point where the navbar changes in px
 
+function checkScroll(){
+  var startY = $('.navbar').height() * 2; 
   if($(window).scrollTop() > startY){
       $('.navbar').addClass("scrolled");
   }else{
       $('.navbar').removeClass("scrolled");
   }
 }
-
 if($('.navbar').length > 0){
   $(window).on("scroll load resize", function(){
       checkScroll();
   });
 }
+
+$(document).ready(function(){
+  $('a[href*="#"]').click(function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+        window.location.hash = hash;
+      });
+    } 
+  });
+});
+
+$(document).ready(function(){
+  $('#contactForm').submit(function(event) {
+    event.preventDefault();
+
+    var name = $('#inputName').val().trim();
+    var email = $('#inputEmail').val().trim();
+    var message = $('#inputMessage').val().trim();
+
+    if (name === '') {
+      alert('Ingresa tu nombre.');
+      return false; 
+    }
+
+    if (email.indexOf('@') === -1) {
+      alert('Ingresa un correo valido.');
+      return false; 
+    }
+
+    if (message === '') {
+      alert('Ingresa tu consulta.');
+      return false;
+    }
+
+    alert('Hemos recibido tu mensaje, te contestaremos pronto!');
+    $('#contactForm')[0].reset();
+  });
+});
